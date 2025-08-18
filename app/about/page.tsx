2029c9c70f67d5128bbcf492b09cced1704b3d7c
EngIltire <iltirepoi@gmail.com>
2025-08-18 17:05:11 +0300

    Updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -56,7 +56,7 @@ export default function AboutPage() {
             className="w-32 h-32 sm:w-40 sm:h-40 relative rounded-full overflow-hidden shadow-md"
           >
             <Image
-              src="/iltire.jpg"
+              src="/eng.jpg"
               alt="Eng Abdalla"
               fill
               className="object-cover"
