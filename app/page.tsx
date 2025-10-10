f32708769e74b86cb4e63ac1524f16b362b2354d
github.com/engabdullah-2024 <enga95311@gmail.com>
2025-10-10 18:32:15 +0300

    last update on my p

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import HeroPage from './hero/page'
-import PostsPage from './posts/page'
+
 
 const page = () => {
   return (
     <div>
       <HeroPage/>
-      <PostsPage/>
+   
     </div>
   )
 }
